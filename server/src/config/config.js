800f02b5fd9156f255a9154cdc5a6dfe598b4419
t3mpV4r <gasoline898@outlook.com>
2019-03-12 15:17:48 +0100

    Login and Register backend

---
+++ server/src/config/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_NAME || 'tabtracker',
+    user: process.env.DB_USER || 'tabtracker',
+    password: process.env.DB_PASS || 'tabtracker',
+    options: {
+      dialect: process.env.DIALECT || 'sqlite',
+      host: process.env.HOST || 'localhost',
+      storage: './tabtracker.sqlite',
+      operatorsAliases: false
+    }
+  },
+  authentication: {
+    jwtSecret: process.env.JWT_SECRET || 'secret'
+  }
+}
---
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -0,0 +1,31 @@
+const Joi = require('joi')
+
+module.exports = {
+  register (req, res, next) {
+    const schema = {
+      email: Joi.string().email(),
+      password: Joi.string().regex(new RegExp('^[a-zA-Z0-9]{8,32}$'))
+    }
+    const { error } = Joi.validate(req.body, schema)
+    if (error) {
+      switch (error.details[0].context.key) {
+        case 'email':
+          res.status(400).send({
+            error: 'You must provide a valid email address.'
+          })
+          break
+        case 'password':
+          res.status(400).send({
+            error: 'The password should follow some rules'
+          })
+          break
+        default:
+          res.status(400).send({
+            error: 'Invalid registration information'
+          })
+      }
+    } else {
+      next()
+    }
+  }
+}
